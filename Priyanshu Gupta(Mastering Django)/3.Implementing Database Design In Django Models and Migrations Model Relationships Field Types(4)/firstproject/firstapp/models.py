2e4c803c731a442b951bea03628cf727bf4a00e3
satyam-seth <satyam1998.1998@gmail.com>
2022-03-06 01:23:46 +0530

    3rd commit

---
+++ Priyanshu Gupta(Mastering Django)/3.Implementing Database Design In Django Models and Migrations Model Relationships Field Types(4)/firstproject/firstapp/models.py
@@ -0,0 +1,43 @@
+from enum import unique
+from itertools import product
+from random import choice
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.product_name
+
+
+class Cart(models.Model):
+    cart_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField()
+
+
+class ProductInCart(models.Model):
+    class Meta:
+        unique_together = (("cart", "product"),)
+
+    product_in_cart_id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+
+class Order(models.Model):
+    status_choice = (
+        (1, "Not Packed"),
+        (2, "Ready For Shipment"),
+        (3, "Shiped"),
+        (4, "Delivered"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=status_choice, default=1)
